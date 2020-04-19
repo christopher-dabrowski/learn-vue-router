0f5e5785917da493a7fc03b09d164d285154cc9a
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-19 19:22:46 +0200

    Display description of destination

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,16 +1,19 @@
 <template>
-  <section class="destination">
-    <!-- <p>The destination ID is: {{ destinationId }}</p> -->
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-detais">
-      <img :src="require(`@/assets/${destination.image}`)" />
+  <section class="destinations">
+    <h1>
+      {{ destination.name }}
+    </h1>
+    <div class="destination-details">
+      <img
+        :src="require(`@/assets/${destination.image}`)"
+        :alt="destination.name"
+      />
+      <p>{{ destination.description }}</p>
     </div>
   </section>
 </template>
-
 <script>
-import store from "@/store.js";
-
+import store from "@/store";
 export default {
   data() {
     return {
@@ -20,7 +23,7 @@ export default {
   computed: {
     destination() {
       return store.destinations.find(
-        (destination) => destination.id == this.destinationId
+        (destination) => destination.id === this.destinationId
       );
     },
   },
@@ -34,12 +37,10 @@ img {
   width: 100%;
   max-height: 400px;
 }
-
 .destination-details {
   display: flex;
   justify-content: space-between;
 }
-
 p {
   margin: 0 40px;
   font-size: 20px;
