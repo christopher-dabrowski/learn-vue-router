d45e03e43e58f2c679a977297269cf44f0aae63e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-19 18:46:23 +0200

    Working details view

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,3 +1,48 @@
 <template>
-  <p>The destination is: {{ this.$route.params.id }}</p>
+  <section class="destination">
+    <!-- <p>The destination ID is: {{ destinationId }}</p> -->
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-detais">
+      <img :src="require(`@/assets/${destination.image}`)" />
+    </div>
+  </section>
 </template>
+
+<script>
+import store from "@/store.js";
+
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+    };
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        (destination) => destination.id == this.destinationId
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
