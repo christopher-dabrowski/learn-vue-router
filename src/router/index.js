37b2dce8edd31876c271b768dd375077b30207bf
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-19 16:58:15 +0200

    Links to destinations

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Brazil from "../views/Brazil.vue";
+import Hawaii from "../views/Hawaii.vue";
+import Panama from "../views/Panama.vue";
+import Jamaica from "../views/Jamaica.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +22,26 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: Brazil
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: Hawaii
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: Panama
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: Jamaica
   }
 ];
 
