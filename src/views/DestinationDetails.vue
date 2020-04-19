b25ad37c0e2326dbc8f9c1d68273f4bd83489fe5
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-19 19:54:06 +0200

    Display experiences in destination

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,16 +1,34 @@
 <template>
-  <section class="destinations">
-    <h1>
-      {{ destination.name }}
-    </h1>
-    <div class="destination-details">
-      <img
-        :src="require(`@/assets/${destination.image}`)"
-        :alt="destination.name"
-      />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
+  <div>
+    <section class="destinations">
+      <h1>
+        {{ destination.name }}
+      </h1>
+      <div class="destination-details">
+        <img
+          :src="require(`@/assets/${destination.image}`)"
+          :alt="destination.name"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <div
+          class="card"
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+        >
+          <img :src="require(`@/assets/${experience.image}`)" />
+          <span class="cart__text">
+            {{ experience.name }}
+          </span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 import store from "@/store";
@@ -49,5 +67,27 @@ p {
   margin: 0 40px;
   font-size: 20px;
   text-align: left;
+}
+
+.cards {
+  display: flex;
+  justify-content: space-between;
+}
+.cards img {
+  max-height: 200px;
+}
+.card {
+  padding: 0 20px;
+  position: relative;
+}
+.card__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
