7b3191e6886698d603ac302e3c62b011776984e4
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-19 16:45:15 +0200

    Resources from tutorial

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h3>Pozdrawiam Magdę</h3>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Pora nauczyć się nowego frameworka 😉" />
   </div>
 </template>
 
