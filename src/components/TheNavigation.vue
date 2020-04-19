27a546d3b0f0d86cbb1697f833a8d1ea8cd5cae8
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-19 18:20:25 +0200

    Move nav styles

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -13,4 +13,18 @@
   display: flex;
   align-items: center;
 }
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
